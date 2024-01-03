649ff02f0586781aa991515fe78ca6044434d310
Drew Helgerson <tah568@msstate.edu>
2024-01-02 19:39:18 -0600

    blueprint

--- avgFloat.cpp
+++
@@ -1,7 +0,0 @@
-float avgFloat(float arr[],int size){
-    float sum = 0;
-    for (int i = 0;i < size;i++){
-        sum += arr[i];
-    }
-    return sum / size;
-}
--- main.cpp
+++ main.cpp
@@ -1,44 +1,20 @@
 #include <iostream>
 #include <string.h>
-#include <vector>
 
 using namespace std;
 
-vector<string> codes(0);
-vector<string> names(0);
+int main(){
 
-bool checkError(int &choice){
-   if (choice == 0) return false;
-   if (choice < 0 || choice > 3){
-      cout << "That's not a valid menu option. Try again.\n";
-   }
-   return true;
 }
 
-void viewSchedule(vector<string> $codes,vector<string> $names){
-   for (int i = 0;i < codes.size();i++){
-      cout << i+1 << ": " << codes.at(i) << " " << names.at(i) << endl;
-   }
-   cout << endl;
+string readContacts() {
+
 }
 
-bool addClass(vector<string> &codes,vector<string> &names){
-   string codec;
-   string name;
-   cout << "What course would you like to add? (4 characters long) ";
-   cin >> 
+void addContacts() {
+
 }
 
-int main() {
-   int choice;
+void displayContacts() {
 
-   do {
-      cout << "0: Exit\n1. View Schedule\n2. Add Class\n3. Remove Class\nWhich menu option would you like? ";
-      cin >> choice;
-      if (choice == 1) viewSchedule();
-      else if (choice == 2) addClass();
-      else if (choice == 3) removeClass();
-   } while (checkError(choice));
-
-   return 0;
 }
